10f5828399399176f5df5d3bdb5b303422ca7dd2
Parichaya Thanawuthikrai <parichaya35@gmail.com>
2026-05-19 23:18:42 +0700

    chore: create invariant testing env

---
+++ test/recon/Setup.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+// Chimera deps
+import {BaseSetup} from "@chimera/BaseSetup.sol";
+import {vm} from "@chimera/Hevm.sol";
+
+// Managers
+import {ActorManager} from "@recon/ActorManager.sol";
+import {AssetManager} from "@recon/AssetManager.sol";
+
+// Helpers
+import {Utils} from "@recon/Utils.sol";
+
+// Your deps
+import "src/Morpho.sol";
+import "src/mocks/OracleMock.sol";
+
+abstract contract Setup is BaseSetup, ActorManager, AssetManager, Utils {
+    Morpho morpho;
+    OracleMock oracleMock;
+    
+    /// === Setup === ///
+    /// This contains all calls to be performed in the tester constructor, both for Echidna and Foundry
+    function setup() internal virtual override {
+        morpho = new Morpho(); // TODO: Add parameters here
+        oracleMock = new OracleMock(); // TODO: Add parameters here
+    }
+
+    /// === MODIFIERS === ///
+    /// Prank admin and actor
+    
+    modifier asAdmin {
+        vm.prank(address(this));
+        _;
+    }
+
+    modifier asActor {
+        vm.prank(address(_getActor()));
+        _;
+    }
+}
