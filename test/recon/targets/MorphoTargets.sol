ec41c97b468ef1e10727262ce321c6ebca0ffa60
Parichaya Thanawuthikrai <parichaya35@gmail.com>
2026-05-21 23:04:44 +0700

    feat: move clamped functions to MorphoTargets and add additional clamped wrappers

--- test/recon/targets/MorphoTargets.sol
+++ test/recon/targets/MorphoTargets.sol
@@ -26,6 +26,71 @@ abstract contract MorphoTargets is
     //     t(!canaryCreateMarket, "morpho_createMarket called");
     // }
 
+    ////// CLAMP FUNCTIONS - Clamp inputs to valid ranges //////
+
+    /// @dev
+    function morpho_borrow_clamped(uint256 assets, uint256 shares, address onBehalf, address receiver) public asActor {
+        morpho_borrow(assets, shares, onBehalf, receiver);
+    }
+
+    /// @dev Clamped function for morpho_createMarket
+    /// @dev This will clamp the marketParams to using our hardcoded tokens
+    /// @dev Hardcoded oracle and irm for simplicity (as now we only have 1 oracle and 1 irm)
+    /// @dev We dont need to pass the whole marketParams as we are clamping it, we only the `lltv`
+    /// @param collatIndex The index of the collateral token (uint8 as we only have 3 tokens)
+    /// @param loanIndex The index of the loan token (uint8 as we only have 3 tokens)
+    /// @param lltv The lltv to clamp
+    function morpho_createMarket_clamped(uint8 collatIndex, uint8 loanIndex, uint256 lltv) public {
+        //@follow-up should we mod the index to be within the range of tokens? so we avoid revert?
+        address collateralToken = _getTokenAt(uint256(collatIndex));  //> this revert if not found
+        address loanToken = _getTokenAt(uint256(loanIndex));          //> this revert if not found
+
+        //@follow-up should we separate this to somewhere?
+        if (!morpho.isLltvEnabled(lltv)) {
+            morpho_enableLltv(lltv);
+        }
+
+        // Create marketParams with clamped values
+        MarketParams memory clampedParams = MarketParams({
+            loanToken: loanToken,
+            collateralToken: collateralToken,
+            oracle: address(mockOracle),
+            irm: address(mockIRM),
+            lltv: lltv
+        });
+        
+        morpho_createMarket(clampedParams);
+    }
+
+    /// @dev Hardcoded empty data for liquidation (no callback triggered)
+    function morpho_liquidate_clamped(address borrower, uint256 seizedAssets, uint256 repaidShares, bytes memory data) public {
+       morpho_liquidate(borrower, seizedAssets, repaidShares, "");
+    }
+
+    /// @dev Hardcoded empty data for liquidation (no callback triggered)
+    function morpho_repay_clamped(uint256 assets, uint256 shares, address onBehalf, bytes memory data) public {
+        morpho_repay(assets, shares, onBehalf, "");
+    }
+
+    /// @dev Hardcoded empty data for liquidation (no callback triggered)
+    function morpho_supply_clamped(uint256 assets, uint256 shares, address onBehalf, bytes memory data) public {
+        morpho_supply(assets, shares, onBehalf, "");
+    }
+
+    /// @dev Hardcoded empty data for liquidation (no callback triggered)
+    function morpho_supplyCollateral_clamped(uint256 assets, address onBehalf, bytes memory data) public {
+        morpho_supplyCollateral(assets, onBehalf, "");
+    }
+
+    function morpho_withdraw_clamped(uint256 assets, uint256 shares, address onBehalf, address receiver) public {
+        morpho_withdraw(assets, shares, onBehalf, receiver);
+    }
+
+    /// @dev Hardcoded empty data for liquidation (no callback triggered)
+    function morpho_withdrawCollateral_clamped(uint256 assets, address onBehalf, address receiver) public {
+        morpho_withdrawCollateral(assets, onBehalf, receiver);
+    }
+
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
     function morpho_accrueInterest() public asActor {
@@ -56,6 +121,7 @@ abstract contract MorphoTargets is
     }
 
     function morpho_flashLoan(address token, uint256 assets, bytes memory data) public asActor {
+        //TODO: the caller need to implement callback, but skip for now
         morpho.flashLoan(token, assets, data);
     }
 
