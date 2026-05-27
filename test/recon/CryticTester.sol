59b84e4ec44d73f89d45729c2fddbdd5703d2fa5
Parichaya Thanawuthikrai <parichaya35@gmail.com>
2026-05-28 01:00:41 +0700

    docs: add recon fuzz command

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -7,6 +7,7 @@ import {TargetFunctions} from "./TargetFunctions.sol";
 
 // echidna . --contract CryticTester --config echidna.yaml --format text --workers 16 --test-limit 1000000
 // medusa fuzz
+// recon fuzz . --contract CryticTester --config echidna.yaml --recon-corpus-dir corpus-recon --format text --workers 16 --test-limit 1000000
 contract CryticTester is TargetFunctions, CryticAsserts {
     constructor() payable {
         setup();
